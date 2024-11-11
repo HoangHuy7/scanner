a4da3a8ffb3292104f502c2ed3353a2ef357b88c
hoanghuy7 <hoanghuy200701@gmail.com>
2024-11-11 17:55:15 +0700

    commit code

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    reactStrictMode: true, // Kích hoạt chế độ Strict Mode trong React
+    swcMinify: true,       // Tăng hiệu suất bằng trình minify của SWC
+    output: "standalone",  // Hữu ích khi deploy trên Render
 };
 
 export default nextConfig;
